0e405307030068b5e7bf11abeb6b42b40da010bd
JestyUY <104555840+JestyUY@users.noreply.github.com>
2023-06-28 06:10:59 +1200

    alejandro list fixed with size and colors

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
- 
+
+
+const menuButton = document.getElementById('menuButton');
+const listaMenu = document.getElementById('listaMenu');
+
+menuButton.addEventListener('click', () => {
+    if (listaMenu.style.display === "none") {
+        listaMenu.style.display = "flex";
+    } else {
+        listaMenu.style.display = "none";
+    }
+});
