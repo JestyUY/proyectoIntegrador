f57b079b262532a290f6c2f04e5990066059995b
JestyUY <104555840+JestyUY@users.noreply.github.com>
2023-06-28 10:46:36 +1200

    button fixed

--- main.js
+++ main.js
@@ -4,9 +4,6 @@ const menuButton = document.getElementById('menuButton');
 const listaMenu = document.getElementById('listaMenu');
 
 menuButton.addEventListener('click', () => {
-    if (listaMenu.style.display === "none") {
-        listaMenu.style.display = "flex";
-    } else {
-        listaMenu.style.display = "none";
-    }
+   listaMenu.classList.toggle('hidden')
 });
+
