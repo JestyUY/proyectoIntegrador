bc168a092659c985bdec77f59e41f13817a61298
JestyUY <104555840+JestyUY@users.noreply.github.com>
2023-06-29 04:23:46 +1200

    last touches

--- main.js
+++ main.js
@@ -1,43 +1,32 @@
+const menuButton = document.getElementById("menuButton");
+const listaMenu = document.getElementById("listaMenu");
 
-
-const menuButton = document.getElementById('menuButton');
-const listaMenu = document.getElementById('listaMenu');
-
-menuButton.addEventListener('click', () => {
-   listaMenu.classList.toggle('hidden')
-   
+menuButton.addEventListener("click", () => {
+  listaMenu.classList.toggle("hidden");
 });
 
-// Obtén el modal
 const modal = document.getElementById("myModal");
 
-// Obtén el botón que abre el modal
 const btn = document.getElementById("buttonIngresar");
 const btnSm = document.getElementById("buttonIngresarSm");
 
-// Obtén el elemento <span> que cierra el modal
+const cruz = document.getElementById("cruz");
 
-const cruz = document.getElementById('cruz')
+cruz.addEventListener("click", () => (modal.style.display = "none"));
 
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
 
-cruz.addEventListener('click', () => modal.style.display = "none")
-// Cierra el modal cuando se hace clic fuera de él
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
-btn.addEventListener('click', ()=> {
-   if (modal.style.display = 'none') {
-      modal.style.display = 'flex'
-   } 
-})
-btnSm.addEventListener('click', ()=> {
-   if (modal.style.display = 'none') {
-      modal.style.display = 'flex'
-   } 
-})
-// btnSm.addEventListener('click', ()=> {
-//    modal.classList.toggle('flex')
-// })
+btn.addEventListener("click", () => {
+  if ((modal.style.display = "none")) {
+    modal.style.display = "flex";
+  }
+});
+btnSm.addEventListener("click", () => {
+  if ((modal.style.display = "none")) {
+    modal.style.display = "flex";
+  }
+});
